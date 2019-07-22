46642a6b32078161fa31dc8b3442d944fc635fa3
Vab <vaib.kapoor15@gmail.com>
2019-07-21 20:28:02 -0400

    Initialized basic state with name and id on each object

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: "Vab",
+          id: "a"
+        },
+        {
+          name: "Alucard",
+          id: "b"
+        },
+        {
+          name: "Roadhog",
+          id: "c"
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
